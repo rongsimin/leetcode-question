1a1d3781477adae9ab18a2b3d9ef6c77b59c092b
rongsimin <747334721@qq.com>
2023-12-23 18:42:05 +0800

    commit MyDev.java in dev

---
+++ src/com/rongsimin/leetcode/editor/cn/MyDev.java
@@ -0,0 +1,11 @@
+package com.rongsimin.leetcode.editor.cn;
+
+/**
+ * @author rsm
+ * @date 2023/12/23 18:37
+ */
+public class MyDev {
+    public static void main(String[] args) {
+        System.out.println("myDEV is run");
+    }
+}
